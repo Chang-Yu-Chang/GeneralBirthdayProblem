8294e72d1c6dc43f74eea39dcca1f94a70df0702
ray57512 <ray57512@gmail.com>
2018-04-18 19:57:10 -0400

    simulateC argument 2

--- R/simulateC.R
+++ R/simulateC.R
@@ -2,7 +2,7 @@
 #' 
 #' @name simulateC
 #' @param k Integer. k persons shared birthday.
-#' @param roomSize Integer. Room size to be tested.
+#' @param room Integer. Room size to be tested.
 #' @param repliate Integer. Replicate size.
 #' @return A floating value indicating the probability of at least k persons sharing birthday in a room
 #' @examples
@@ -13,8 +13,8 @@
 #'
 #'
 
-simulateC <- function(k=2, roomSize=23, replicate=1e5) {
-  .Call(`_GeneralBirthdayProblem_simulateC`, k=k, roomSize=roomSize, replicate=replicate)
+simulateC <- function(k=2, room=23, replicate=1e5) {
+  .Call(`_GeneralBirthdayProblem_simulateC`, k=k, room=room, replicate=replicate)
 }
 
 
